04804f5b2e26c25464ecf6fa37a285f265a28486
tonyfast <tony.fast@gmail.com>
2019-03-25 20:42:26 -0400

    Move the test configs to the formatters module for default settings.

--- src/annodize/__init__.py
+++ src/annodize/__init__.py
@@ -1,2 +1,15 @@
+__all__ = 'context',
+
 with __import__('importnb').Notebook():
-    from .annodize import context, load_ipython_extension, unload_ipython_extension
+    from .annodize import context
+    
+
+def load_ipython_extension(shell):
+    annodize.load_ipython_extension(shell)
+    from . import formatters
+    formatters.load_ipython_extension(shell)
+    
+def unload_ipython_extension(shell):
+    annodize.unload_ipython_extension(shell)
+    from . import formatters
+    formatters.unload_ipython_extension(shell)
---
+++ src/annodize/formatters/__init__.py
@@ -0,0 +1,3 @@
+import importnb
+with importnb.Notebook():
+    from .base import load_ipython_extension, unload_ipython_extension
