1b73499f350f6dcf3e27c7752744d4bc026870b6
tonyfast <tony.fast@gmail.com>
2019-03-27 08:03:26 -0400

    Update __init__.py
    remove magic invocation to normal python

--- src/annodize/__init__.py
+++ src/annodize/__init__.py
@@ -2,14 +2,3 @@ __all__ = 'context',
 
 with __import__('importnb').Notebook():
     from .annodize import context, app
-    
-
-def load_ipython_extension(shell):
-    annodize.load_ipython_extension(shell)
-    from . import formatters
-    formatters.load_ipython_extension(shell)
-    
-def unload_ipython_extension(shell):
-    annodize.unload_ipython_extension(shell)
-    from . import formatters
-    formatters.unload_ipython_extension(shell)
