e5e57553bbfdb319f5dbdcfad90d36d2a4775cad
tonyfast <tony.fast@gmail.com>
2019-04-05 10:13:41 -0400

    update

--- src/annodize/__init__.py
+++ src/annodize/__init__.py
@@ -1,4 +1,13 @@
 __all__ = 'context',
 
 with __import__('importnb').Notebook():
-    from .annodize import context, app
+    from .annodize import Interface, Function, Union, U
+    from . import annodize, formatters, tracking
+    
+def load_ipython_extension(shell):
+    tracking.load_ipython_extension(shell)
+    formatters.load_ipython_extension(shell)
+    
+def unload_ipython_extension(shell):
+    tracking.unload_ipython_extension(shell)
+    formatters.unload_ipython_extension(shell)
--- src/annodize/formatters/__init__.py
+++
@@ -1,3 +0,0 @@
-import importnb
-with importnb.Notebook():
-    from .base import load_ipython_extension, unload_ipython_extension
